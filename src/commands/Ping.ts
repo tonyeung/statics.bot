d4489c83b64ab7c6d6c523f904235a8b8bf3fb4c
ton yeung <rhduong@uh.edu>
2020-07-26 04:20:06 +0000

    implemented guard

--- src/commands/Ping.ts
+++ src/commands/Ping.ts
@@ -1,9 +1,11 @@
-import { Command, CommandMessage } from "@typeit/discord";
+import { Command, CommandMessage, Guard } from "@typeit/discord";
+import { Say } from "../guards/Say";
 
 export abstract class Ping {
     @Command("ping")
+    @Guard(Say("guard"))
     async ping(command: CommandMessage): Promise<void> {
-        console.log("!ping called");
+        console.log("!ping called with guard");
         command.reply("Pong!");
     }
 }
